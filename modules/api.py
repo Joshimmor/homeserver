ae406bb092c3a4d8110d76006510889b7638e734
Joshimmor <Joshimmor@gmail.com>
2023-01-30 21:20:21 -0500

    init home server

--- modules/api.py
+++ modules/api.py
@@ -5,10 +5,24 @@
 
 # Import Libraries 
 from app import app
-from flask import jsonify
+from flask import jsonify , request
+from redis import Redis
+from rq import Queue
+
+connection = Redis()
+q = Queue(connection)
 
 # Define route "/api".
 @app.route('/api')
 def api():
   # return in JSON format. (For API)
   return jsonify({"message":"Hello from Flask!"})
+
+@app.route('/tank',methods=['GET'])
+def get_tank():
+    return jsonify({"tempature":90.5,"humidity":60})
+@app.route('/tank', methods=['POST'])
+def post_tank():
+    temp = request.get_json()
+    
+    return temp
